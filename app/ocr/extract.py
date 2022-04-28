9e03be1a1e49f39870b0a205476ceec16bff0f74
Aman Shitta <amanshitta@crossml.com>
2022-04-28 19:08:04 +0530

    OCR changes and extract added

--- app/ocr/extract.py
+++ app/ocr/extract.py
@@ -1,9 +1,17 @@
 from lib2to3.pytree import convert
 from unittest import result
+from urllib.request import DataHandler
 from doctr.io import DocumentFile
 from doctr.models import ocr_predictor
 from PIL import Image
 import os
+from fuzzywuzzy import fuzz
+from config import AZKEY1, DATA, AZKEY2, ENDPOINT, rearrange_output
+
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
 
 DOCTR_MODEL = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_vgg16_bn', pretrained=True)
 
@@ -12,99 +20,133 @@ class Extractor:
     def __init__(self, document):
         
         self.document = document
+        self.doc_words = []
         self.output = {}
     
-    def convert_tif_to_pages(self, doc):
-        document_images = []
-        try:
-            img = Image.open(doc)
-            total_pages = img.n_frames
-            if total_pages > 0:
-                # create directory for document
-                images_path = os.path.splitext(doc)[0]
-                filename_split = os.path.basename(doc).split(".")
-                os.makedirs(images_path)
 
-                for i in range(total_pages):
-                    try:
-                        img.seek(i)
-                        out_file = ".".join(filename_split[:-1]) + "_" + str(i) + "." + filename_split[-1]
-                        img_path = os.path.join(images_path, out_file)
-                        img.save(img_path)
-                        document_images.append(img_path)
+    def generate_ngrams(self, words, n_range=2):
+        ngrams = []
+        for i in range(0, len(words)-(n_range-1)):
+            ngram_words = words[i:i+n_range]
+            ngram_text = " ".join([n_word[1] for n_word in ngram_words])
+            n_x1, n_y1, n_x2, n_y2 =  ngram_words[0][0][0], ngram_words[0][0][1], ngram_words[-1][0][2], ngram_words[-1][0][3]
+            # ngram_cs = sum([n_word[2] for n_word in ngram_words]) / len(ngram_words)
+            ngrams.append((
+                [n_x1, n_y1, n_x2, n_y2],
+                ngram_text
+            ))
 
-                        # create a PNG version of tif file for frontend use
-                        if filename_split[-1].lower() != 'png':
-                            png_file = ".".join(filename_split[:-1]) + "_" + str(i) + ".png"
-                            img_path = os.path.join(images_path, png_file)
-                            img.save(img_path)
-                    except EOFError:
-                        break
-        except Exception as e:
-            print(e)
+        return ngrams
 
-        return document_images
+    def get_radius_text(self, cords, words):
+        max_word_x = max([word[0][2] for word in words])
 
-    def convert_doctr_to_words(self, json_output):
-        op = {}
-        for pi,page in enumerate(json_output['pages']):
-            words = []
-            for block in page['blocks']:
-                for line in block['lines']:
-                    for word in line['words']:
-                        words.append((word['geometry'], word['value'], word['confidence']*100))
-            op.update({pi:words}) 
+        left = cords[0] - int(max_word_x*0.02)
+        top = cords[1] - int(max_word_x*0.02)
+        right = cords[2] + int(max_word_x*0.2)
+        bottom = cords[3] + int(max_word_x*0.08)
+        
+        from PIL import Image, ImageDraw
+        img = Image.open('image.jpg')
+        dr = ImageDraw.Draw(img)
+        dr.rectangle((left, top, right, bottom), outline="green", width=2)
+        img.save('rad.png')
 
-        return op
+        radius_words = []
+        for word in words:
+            if word[0][0] > left and \
+                word[0][1] > top and  \
+                    word[0][2] < right  and \
+                        word[0][3] < bottom:
+                        radius_words.append(word)
 
-    def doctr_ocr(self, doc, doc_type):
-        # PDF
-        pages = {}
-        if doc_type == 'pdf':
-            doc_file = DocumentFile.from_pdf(doc)
-            result = DOCTR_MODEL(doc_file)
-            json_output = result.export()
-            page_words = self.convert_doctr_to_words(json_output)
+        return ' '.join(w[1] for w in radius_words)
+
+    def find_key_value(self, key, words):
+        matches = {}
+        key_len = len(key.split())
+        n_grams = self.generate_ngrams(words, key_len)
+        for idx,label in enumerate(n_grams):
+            if fuzz.ratio(label[1].lower().strip(), key.lower().strip()) > 85:
+                matches.update({idx:label})
+
+        if not matches:
+            breakpoint()
             
+        for index,matched_label in matches.items():
+            radius_text = self.get_radius_text(matched_label[0], words[index:])     
+            # breakpoint()
 
-        # Image
-        elif doc_type in ['jpg', 'jpeg']:
-            doc_file = DocumentFile.from_images(doc)
-            result = DOCTR_MODEL(doc_file)
-            json_output = result.export()
-            page_words = self.convert_doctr_to_words(json_output)
-            
-        else:
-            print(f"{doc_type} Format not supported yet !")
-        # TIFF image
-        # elif doc_type == 'tiff':
-        #     pages = {}
-        #     image_pages = self.convert_tif_to_pages(doc)
-        #     for page,image in enumerate(image_pages):
-        #         result = DocumentFile.from_images(image)
-        #         json_output = result.export()
-        #         page_words = self.convert_doctr_to_words(json_output)
-        #         pages.update({page+1:page_words})
+            print(radius_text)
 
-        self.output = page_words
-
-    def identify_doc_type(self, doc):
-        file_n = os.path.splitext(doc)[-1]
-        if file_n.endswith('jpeg') or file_n.endswith('jpg'):
-            return "jpeg"
-        if file_n.endswith('pdf'):
-            return "pdf"
-
-    def extract(self):
-        doc = self.document
-        doc_type = self.identify_doc_type(doc)
-        self.doctr_ocr(doc, doc_type=doc_type)
+    def extract(self, words):
+        # self.doc_words = rearrange_output(ocr_data)
+        self.find_key_value("First Name", words)
+        # doc = self.document
+        # self.get_radius_text()
         return self.output
 
-if __name__ == "__main__":
-    path = "/home/aman/Downloads/sample.pdf"
-    # path = "/home/aman/Downloads/Aman_Shitta_Resume_26-02-2022-22-13-48.pdf"
 
-    e = Extractor(path)
-    print(e.extract())
+if __name__ == "__main__":
+    import time
+
+    """ AZURE OCR CODE """
+
+    subscription_key = AZKEY1
+    computervision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(subscription_key))
+    
+    images_folder = "/home/attihs/Downloads"
+
+    # Get image path
+    read_image_path = os.path.join (images_folder, "image.jpg")
+    # Open the image
+
+    read_image = open(read_image_path, "rb")
+
+    # Call API with image and raw response (allows you to get the operation location)
+    read_response = computervision_client.read_in_stream(read_image, raw=True)
+
+    # Get the operation location (URL with ID as last appendage)
+    read_operation_location = read_response.headers["Operation-Location"]
+    # Take the ID off and use to get results
+    operation_id = read_operation_location.split("/")[-1]
+
+    # Call the "GET" API and wait for the retrieval of the results
+    while True:
+        read_result = computervision_client.get_read_result(operation_id)
+        if read_result.status.lower () not in ['notstarted', 'running']:
+            break
+        print ('Waiting for result...')
+        time.sleep(10)
+
+    # Print results, line by line
+    pages_data = {}
+    if read_result.status == OperationStatusCodes.succeeded:
+        # data = read_result.analyze_result.read_results.as_dict
+        for text_result in read_result.analyze_result.read_results:
+            data = text_result.as_dict()
+            # for line in text_result.lines:
+            #     print(line.text)
+            #     print(line.bounding_box)
+            pages_data.update({data['page']:rearrange_output(data)})
+    
+    
+    """ Draw box for words"""
+    # from PIL import Image, ImageDraw
+    # img = Image.open(read_image_path)
+    # dr = ImageDraw.Draw(img)
+
+    # for _,page_words in pages_data.items():
+    #     for word in page_words:
+    #         dr.rectangle(word[0], outline="red", width=3)
+    # img.save('reg.png')
+
+
+    """ TESTING FURTHER """
+    # pages_data = {}
+    # pages_data.update({DATA['page']:rearrange_output(DATA)})
     # breakpoint()
+    ext = Extractor('path')
+    for page, words in pages_data.items():
+        print("[+] Finding at Page :: ", page)
+        ext.find_key_value("First Name", words)
